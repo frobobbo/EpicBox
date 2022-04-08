68a6bd0e0bfa46fc82a61456603c7826ae140b7c
frobobbo <johnsobd@gmail.com>
2022-04-08 15:57:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,4 +44,4 @@ sudo python ~/RaspiWiFi/initial_setup.py
 sudo sed -i -e '$i \sleep 10 \n' /etc/rc.local
 sudo sed -i -e '$i \sh /home/pi/EpicBox/Setup/startup.sh > /home/pi/EpicBox/Setup/startuplog 2>&1 \n' /etc/rc.local
 
-sudo systemctl unmask hostpad
+sudo systemctl unmask hostapd
