1030536363c26aedd09f20993039e83b83f52cdc
Brett Johnson <johnsobd@gmail.com>
2022-06-01 22:04:29 -0400

    change permissions

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ git clone https://github.com/frobobbo/RaspiWiFi.git
 sudo python ~/RaspiWiFi/initial_setup.py
 
 sudo sed -i -e '$i \sleep 10 \n' /etc/rc.local
-sudo sed -i -e '$i \sh /home/pi/EpicBox/Setup/startup.sh > /home/pi/EpicBox/Setup/startuplog 2>&1 \n' /etc/rc.local
+sudo sed -i -e '$i \sudo -u pi sh /home/pi/EpicBox/Setup/startup.sh > /home/pi/EpicBox/Setup/startuplog 2>&1 \n' /etc/rc.local
 
 sudo systemctl unmask hostapd
 sudo systemctl disable getty@tty1
